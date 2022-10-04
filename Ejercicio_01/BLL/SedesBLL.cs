ffadec981f652fd257d7ad217ee4d44b93576a6b
Michelle Arèvalo <emelymichellearlo@gmail.com>
2022-10-03 21:24:16 -0600

    S

---
+++ Ejercicio_01/BLL/SedesBLL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio_01.BLL
+{
+    class SedesBLL
+    {
+        public int Id {get; set;}
+        public string Nombre { get; set; }
+        public string Ubicacion { get; set; }
+
+
+    }
+}
---
+++ Ejercicio_01/DAL/Database.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ejercicio_01.Properties;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Ejercicio_01.DAL
+{
+    public class Database
+    {
+        public static string getStrConnecction()
+        {
+            return Settings.Default.empleadosConnection;
+        }
+
+        public SqlConnection geetConnectionn()
+        {
+            SqlConnection Con = new SqlConnection(getStrConnecction());
+            return Con;
+        }
+
+
+    }
+}
